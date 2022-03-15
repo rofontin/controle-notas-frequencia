706d820b47cb71e154bb0d2eff9f33be73d0c8a2
Riquelmy Fontin <fontinriquelmy@gmail.com>
2022-03-14 23:37:51 -0300

    alteracao de cores e criacao da tela inicial.

--- app/src/main/java/com/example/controlenotasfrequencia/MainActivity.java
+++ app/src/main/java/com/example/controlenotasfrequencia/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.controlenotasfrequencia;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.controlenotasfrequencia.cadastroTurma.ListaTurmaActivity;
 import com.example.controlenotasfrequencia.cadastroaluno.ListaAlunoActivity;
+import com.example.controlenotasfrequencia.cadastrodisciplina.ListaDisciplinaActivity;
+import com.example.controlenotasfrequencia.cadastroprofessor.ListaProfessorActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +19,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void listarAlunos(View view) {
+        Intent intent = new Intent(this, ListaAlunoActivity.class);
+        startActivity(intent);
+    }
 
-    public void cadastrarAluno(View view) {
+    public void listarProfessores(View view) {
+        Intent intent = new Intent(this, ListaProfessorActivity.class);
+        startActivity(intent);
+    }
+
+    public void listarTurmas(View view) {
+        Intent intent = new Intent(this, ListaTurmaActivity.class);
+        startActivity(intent);
+    }
+
+    public void listarDisciplinas(View view) {
+        Intent intent = new Intent(this, ListaDisciplinaActivity.class);
+        startActivity(intent);
+    }
+
+    public void listarNotasFrequencia(View view) {
         Intent intent = new Intent(this, ListaAlunoActivity.class);
         startActivity(intent);
     }
